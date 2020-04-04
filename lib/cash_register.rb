c6744981e0dbfaaad5da077f64a9ac49027fb06a
matthew enbar <matthew.enbar@gmail.com>
2020-04-04 15:37:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,9 +15,9 @@ class CashRegister
 
   def apply_discount
     #binding.pry
-    @discount = ((100 - discount) / 100 )
+    @discount = ((100.0 - discount.to_f) / 100.0 )
     @total = total * discount
-    return "After the discount, the total comes to #{@total}."
+    return "After the discount, the total comes to #{@total.to_i}."
   end
 
 
