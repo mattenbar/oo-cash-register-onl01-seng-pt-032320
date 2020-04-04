63f0af547536321eee1a9393b8190d9f3a5d4cae
matthew enbar <matthew.enbar@gmail.com>
2020-04-04 15:07:25 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,7 @@ class CashRegister
     @discount = discount
   end
 
-  def add_item(title, price, item_amount)
+  def add_item(title, price, item_amount = 1)
     @total = total.to_f + (price.to_f * item_amount.to_i)
 
   end
