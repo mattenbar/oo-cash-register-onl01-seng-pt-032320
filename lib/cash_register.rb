4ad1c5adcb2bbf79dde8f1dc5f713606369efe29
matthew enbar <matthew.enbar@gmail.com>
2020-04-04 15:28:54 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -5,7 +5,7 @@ class CashRegister
 
   def initialize(discount = 0)
     @total = total = 0
-    @discount = discount.to_int
+    @discount = discount.to_i
   end
 
   def add_item(title, price, item_amount = 1)
