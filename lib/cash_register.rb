1bf0c07beaaf441ccbd7a4efc6c3daf9fbe1c5c0
matthew enbar <matthew.enbar@gmail.com>
2020-04-04 15:00:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,11 +4,13 @@ class CashRegister
   attr_accessor :total, :discount
 
   def initialize(discount = 0)
-    @total = 0
+    @total = total = 0
     @discount = discount
   end
 
+  def add_item(title, price)
 
+  end
 
 
 
